317648e1b5103d47ccf00cc53acfe7e045901e3d
Mubasshir <mahammad.mubassir@gmail.com>
2023-06-27 22:53:26 +0530

    Updated

--- app/src/main/java/com/example/thanal_invoice/InvoiceActivity.java
+++ app/src/main/java/com/example/thanal_invoice/InvoiceActivity.java
@@ -1,10 +1,14 @@
 package com.example.thanal_invoice;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.Build;
@@ -13,10 +17,12 @@ import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,11 +52,13 @@ public class InvoiceActivity extends AppCompatActivity {
 
     private ImageView logoImageView;
     private TextView totalAmountTextView;
-    private EditText itemEditText;
+    private Spinner itemSpinner;
     private EditText quantityEditText;
     private EditText rateEditText;
     private Button savePdfButton;
     private ListView itemListView;
+
+    private EditText clientEditText;
 
     private ArrayList<InvoiceItem> invoiceItems;
     private InvoiceItemAdapter itemAdapter;
@@ -58,6 +66,7 @@ public class InvoiceActivity extends AppCompatActivity {
     private ActivityResultLauncher<String> requestPermissionLauncher;
     private ActivityResultLauncher<Intent> createFileLauncher;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,11 +75,15 @@ public class InvoiceActivity extends AppCompatActivity {
         // Initialize views
         logoImageView = findViewById(R.id.logoImageView);
         totalAmountTextView = findViewById(R.id.totalAmountTextView);
-        itemEditText = findViewById(R.id.itemEditText);
+        itemSpinner = findViewById(R.id.spinner_item);
         quantityEditText = findViewById(R.id.quantityEditText);
         rateEditText = findViewById(R.id.rateEditText);
         savePdfButton = findViewById(R.id.savePdfButton);
         itemListView = findViewById(R.id.itemListView);
+        clientEditText = findViewById(R.id.clientEditText);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.item_list, android.R.layout.simple_spinner_item);
+        itemSpinner.setAdapter(adapter);
 
         // Set the logo image
         Bitmap logoBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
@@ -85,7 +98,33 @@ public class InvoiceActivity extends AppCompatActivity {
         findViewById(R.id.addItemButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String item = itemEditText.getText().toString();
+                // Validate client name
+                String clientName = clientEditText.getText().toString().trim();
+                if (clientName.isEmpty()) {
+                    clientEditText.setError("Client name is required.");
+                    return;
+                }
+
+                // Validate spinner selection
+                String spinnerItem = itemSpinner.getSelectedItem().toString();
+                if (spinnerItem.isEmpty()) {
+                    Toast.makeText(InvoiceActivity.this, "Please select an item.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String Quantity = quantityEditText.getText().toString().trim();
+                if (Quantity.isEmpty()) {
+                    quantityEditText.setError("Quantity is required.");
+                    return;
+                }
+
+                String Rate = rateEditText.getText().toString().trim();
+                if (Rate.isEmpty()) {
+                    rateEditText.setError("Rate is required.");
+                    return;
+                }
+
+                String item = itemSpinner.getSelectedItem().toString();
                 int quantity = Integer.parseInt(quantityEditText.getText().toString());
                 double rate = Double.parseDouble(rateEditText.getText().toString());
 
@@ -103,7 +142,6 @@ public class InvoiceActivity extends AppCompatActivity {
                 totalAmountTextView.setText(String.format("Total Amount: Rs%.2f", totalAmount));
 
                 // Clear input fields
-                itemEditText.setText("");
                 quantityEditText.setText("");
                 rateEditText.setText("");
             }
@@ -193,7 +231,7 @@ public class InvoiceActivity extends AppCompatActivity {
                 FileOutputStream fileOutputStream = new FileOutputStream(pfd.getFileDescriptor());
 
                 // Create a new document
-                Document document = new Document();
+                com.itextpdf.text.Document document = new com.itextpdf.text.Document();
 
                 // Set page size and margins
                 document.setPageSize(PageSize.A4);
@@ -213,19 +251,27 @@ public class InvoiceActivity extends AppCompatActivity {
                 com.itextpdf.text.Image logoImage = com.itextpdf.text.Image.getInstance(byteArray);
                 logoImage.scaleToFit(100, 100);
                 document.add(logoImage);
-                document.add(new Paragraph("Your Business Name", FontFactory.getFont(FontFactory.HELVETICA_BOLD, 20)));
+                document.add(new com.itextpdf.text.Paragraph("THANAL EVENTS SARALIKATTE", FontFactory.getFont(FontFactory.HELVETICA_BOLD, 20)));
+
+                // Add the client name
+                String clientName = clientEditText.getText().toString();
+                document.add(new com.itextpdf.text.Paragraph("Client Name: " + clientName));
+
+                // Add the spinner item
+                String spinnerItem = itemSpinner.getSelectedItem().toString();
+                document.add(new com.itextpdf.text.Paragraph("Item: " + spinnerItem));
 
                 // Add the invoice items
                 for (InvoiceItem item : invoiceItems) {
-                    document.add(new Paragraph(item.getItem()));
-                    document.add(new Paragraph("Quantity: " + item.getQuantity()));
-                    document.add(new Paragraph("Rate: $" + item.getRate()));
-                    document.add(new Paragraph("Price: $" + item.getPrice()));
-                    document.add(new Paragraph("\n"));
+                    document.add(new com.itextpdf.text.Paragraph(item.getItem()));
+                    document.add(new com.itextpdf.text.Paragraph("Quantity: " + item.getQuantity()));
+                    document.add(new com.itextpdf.text.Paragraph("Rate: Rs " + item.getRate()));
+                    document.add(new com.itextpdf.text.Paragraph("Price: Rs " + item.getPrice()));
+                    document.add(new com.itextpdf.text.Paragraph("\n"));
                 }
 
                 // Add the total amount
-                document.add(new Paragraph("Total Amount: $" + calculateTotalAmount()));
+                document.add(new com.itextpdf.text.Paragraph("Total Amount: Rs " + calculateTotalAmount()));
 
                 // Close the document
                 document.close();
@@ -239,8 +285,6 @@ public class InvoiceActivity extends AppCompatActivity {
         }
     }
 
-
-
     private void savePdfLegacy() {
         // Create a new document
         PdfDocument document = new PdfDocument();
@@ -252,25 +296,98 @@ public class InvoiceActivity extends AppCompatActivity {
         PdfDocument.Page page = document.startPage(pageInfo);
 
         // Create a canvas for drawing
-        android.graphics.Canvas canvas = page.getCanvas();
+        Canvas canvas = page.getCanvas();
 
         // Add the header (business name and logo)
         Bitmap logoBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
-        canvas.drawBitmap(logoBitmap, 50, 50, null);
-        canvas.drawText("Your Business Name", 200, 100, null);
+        int logoWidth = logoBitmap.getWidth();
+        int logoHeight = logoBitmap.getHeight();
+        int centerX = pageInfo.getPageWidth() / 2;
+        int logoLeft = centerX - (logoWidth / 2);
+        int logoTop = 50;
+        canvas.drawBitmap(logoBitmap, logoLeft, logoTop, null);
+
+        // Set font style for the header
+        Paint headerPaint = new Paint();
+        headerPaint.setTextSize(16f);
+        headerPaint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+
+        // Add business name
+        String businessName = "THANAL EVENTS";
+        float businessNameWidth = headerPaint.measureText(businessName);
+        int businessNameLeft = centerX - (int) (businessNameWidth / 2);
+        int businessNameTop = logoTop + logoHeight + 30;
+        canvas.drawText(businessName, businessNameLeft, businessNameTop, headerPaint);
+
+        // Set font style for address and contact details
+        Paint detailsPaint = new Paint();
+        detailsPaint.setTextSize(12f);
+
+        // Add business address
+        String businessAddress = "SARALIKATTE, THEKKAR POST & VILLAGE, BELTHANGADY TALUK";
+        float businessAddressWidth = detailsPaint.measureText(businessAddress);
+        int businessAddressLeft = centerX - (int) (businessAddressWidth / 2);
+        int businessAddressTop = businessNameTop + 20;
+        canvas.drawText(businessAddress, businessAddressLeft, businessAddressTop, detailsPaint);
+
+        // Add business contact
+        String businessContact = "+91 9880478873";
+        float businessContactWidth = detailsPaint.measureText(businessContact);
+        int businessContactLeft = centerX - (int) (businessContactWidth / 2);
+        int businessContactTop = businessAddressTop + 20;
+        canvas.drawText(businessContact, businessContactLeft, businessContactTop, detailsPaint);
+
+        // Add the client name
+        String clientName = clientEditText.getText().toString();
+        canvas.drawText("Client Name: " + clientName, 50, businessContactTop + 30, null);
+
+        // Set font style for the table
+        Paint tablePaint = new Paint();
+        tablePaint.setTextSize(14f);
+
+        // Define table column widths
+        int serialNumberColumn = 50;
+        int itemColumn = 200;
+        int quantityColumn = 350;
+        int rateColumn = 450;
+        int priceColumn = 550;
+
+        // Draw table headers
+        canvas.drawText("Serial No.", serialNumberColumn, businessContactTop + 80, tablePaint);
+        canvas.drawText("Item", itemColumn, businessContactTop + 80, tablePaint);
+        canvas.drawText("Quantity", quantityColumn, businessContactTop + 80, tablePaint);
+        canvas.drawText("Rate", rateColumn, businessContactTop + 80, tablePaint);
+        canvas.drawText("Price", priceColumn, businessContactTop + 80, tablePaint);
 
         // Add the invoice items
-        int y = 200;
+        int y = businessContactTop + 120;
+        int serialNumber = 1;
         for (InvoiceItem item : invoiceItems) {
-            canvas.drawText(item.getItem(), 50, y, null);
-            canvas.drawText("Quantity: " + item.getQuantity(), 50, y + 30, null);
-            canvas.drawText("Rate: $" + item.getRate(), 50, y + 60, null);
-            canvas.drawText("Price: $" + item.getPrice(), 50, y + 90, null);
-            y += 120;
+            // Draw serial number
+            canvas.drawText(String.valueOf(serialNumber), serialNumberColumn, y, tablePaint);
+
+            // Draw item
+            canvas.drawText(item.getItem(), itemColumn, y, tablePaint);
+
+            // Draw quantity
+            canvas.drawText(String.valueOf(item.getQuantity()), quantityColumn, y, tablePaint);
+
+            // Draw rate
+            canvas.drawText("Rs " + item.getRate(), rateColumn, y, tablePaint);
+
+            // Draw price
+            canvas.drawText("Rs " + item.getPrice(), priceColumn, y, tablePaint);
+
+            y += 30;
+            serialNumber++;
         }
 
         // Add the total amount
-        canvas.drawText("Total Amount: $" + calculateTotalAmount(), 50, y, null);
+        String totalAmount = "Total Amount: Rs " + calculateTotalAmount();
+        Paint totalPaint = new Paint();
+        totalPaint.setTextSize(14f);
+        totalPaint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        canvas.drawText(totalAmount, 50, y + 30, totalPaint);
 
         // Finish the page
         document.finishPage(page);
@@ -289,4 +406,5 @@ public class InvoiceActivity extends AppCompatActivity {
             Toast.makeText(this, "Failed to save PDF.", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
