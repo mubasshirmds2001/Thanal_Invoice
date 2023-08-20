223dcdc5e14706a343472489f694aca7347d472b
Mubasshir <mahammad.mubassir@gmail.com>
2023-08-20 11:11:57 +0530

    Modified code and PDF layouts

--- app/src/main/java/com/example/thanal_invoice/InvoiceActivity.java
+++ app/src/main/java/com/example/thanal_invoice/InvoiceActivity.java
@@ -30,19 +30,25 @@ import androidx.core.content.FileProvider;
 import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.io.source.ByteArrayOutputStream;
+import com.itextpdf.kernel.colors.Color;
+import com.itextpdf.kernel.colors.DeviceRgb;
 import com.itextpdf.kernel.events.Event;
 import com.itextpdf.kernel.events.IEventHandler;
 import com.itextpdf.kernel.events.PdfDocumentEvent;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.draw.SolidLine;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.borders.Border;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Div;
 import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.LineSeparator;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.HorizontalAlignment;
 import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.UnitValue;
 import com.itextpdf.layout.property.VerticalAlignment;
@@ -51,6 +57,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
@@ -152,18 +159,14 @@ public class InvoiceActivity extends AppCompatActivity {
             // Clear input fields
             quantityEditText.setText("");
             rateEditText.setText("");
-            quantityEditText.requestFocus();
+            spinner_item.requestFocus();
 
             double grandTotal = 0.0;
 
-            // Update the totalAmount variable by adding the price of the current item
-            grandTotal += Double.parseDouble(Price);
-
-            // Set the totalAmount in the TextView
-            totalAmountTextView.setText("Grand Total :" + String.valueOf(grandTotal));
-
-            ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+            for (InvoiceItem item : invoiceItems) {
+                grandTotal += item.getPrice();
+            }
+            totalAmountTextView.setText("Grand Total: " + String.valueOf(grandTotal));
         });
 
         saveListButton.setOnClickListener(new View.OnClickListener() {
@@ -184,6 +187,7 @@ public class InvoiceActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Clear the invoiceItems list
                 invoiceItems.clear();
+                totalAmountTextView.setText("");
 
                 // Notify the adapter of the data set change
                 itemAdapter.notifyDataSetChanged();
@@ -192,63 +196,54 @@ public class InvoiceActivity extends AppCompatActivity {
     }
 
     private void generatePDF() throws IOException {
+        int templateResourceId = R.raw.custompdf;
+        InputStream inputStream = getResources().openRawResource(templateResourceId);
+        PdfReader templateReader = new PdfReader(inputStream);
+
         String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
         File file = new File(pdfPath, "Invoice.pdf");
         OutputStream outputStream = new FileOutputStream(file);
 
 
         PdfWriter writer = new PdfWriter(file);
-        PdfDocument pdfdocument = new PdfDocument(writer);
+        PdfDocument pdfdocument = new PdfDocument(templateReader,writer);
         Document document1 = new Document(pdfdocument);
 
-        pdfdocument.setDefaultPageSize(PageSize.A4);
-        document1.setMargins(0,0,0,0);
+        String clientName = clientEditText.getText().toString();
+        Paragraph labelParagraph = new Paragraph("Client Name: "+clientName)
+                .setBold().setFontSize(18).setMarginTop(130).setFontColor(new DeviceRgb(139, 0, 0));
 
-        Drawable drawable = getDrawable(R.drawable.header);
-        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
-        byte[] bitmapdata = stream.toByteArray();
-
-        ImageData imageData = ImageDataFactory.create(bitmapdata);
-        Image image = new Image(imageData);
-
-        Table headerTable = createHeaderTable();
-
-        // Set the position of the header
-        pdfdocument.addEventHandler(PdfDocumentEvent.START_PAGE, event -> {
-            float x = document1.getLeftMargin();
-            float y = document1.getTopMargin();
-            headerTable.setFixedPosition(x, y, PageSize.A4.getWidth());
-        });
-
-        // Add the headerTable to each page
-        pdfdocument.addEventHandler(PdfDocumentEvent.END_PAGE, event -> {
-            document1.add(headerTable);
-        });
+        document1.add(labelParagraph);
+        document1.add(new Paragraph("\n")); // Blank space
 
         // Add table header
-        Table table = new Table(new float[]{1, 3, 2, 2, 2});
-        table.addHeaderCell(new Cell().add(new Paragraph("Sl.No.").setTextAlignment(TextAlignment.CENTER)));
-        table.addHeaderCell(new Cell().add(new Paragraph("Item Name").setTextAlignment(TextAlignment.CENTER)));
-        table.addHeaderCell(new Cell().add(new Paragraph("Quantity").setTextAlignment(TextAlignment.CENTER)));
-        table.addHeaderCell(new Cell().add(new Paragraph("Rate").setTextAlignment(TextAlignment.CENTER)));
-        table.addHeaderCell(new Cell().add(new Paragraph("Total Amount").setTextAlignment(TextAlignment.CENTER)));
+        Table table = new Table(new float[]{1, 3, 2, 2, 2}).setMarginTop(3);
+        Color headerBackgroundColor = new DeviceRgb(0, 0, 255);
+        table.addHeaderCell(new Cell().add(new Paragraph("Sl.No.").setFontColor(headerBackgroundColor).setTextAlignment(TextAlignment.CENTER)));
+        table.addHeaderCell(new Cell().add(new Paragraph("Item Name").setFontColor(headerBackgroundColor).setTextAlignment(TextAlignment.CENTER)));
+        table.addHeaderCell(new Cell().add(new Paragraph("Quantity").setFontColor(headerBackgroundColor).setTextAlignment(TextAlignment.CENTER)));
+        table.addHeaderCell(new Cell().add(new Paragraph("Rate").setFontColor(headerBackgroundColor).setTextAlignment(TextAlignment.CENTER)));
+        table.addHeaderCell(new Cell().add(new Paragraph("Total Amount").setFontColor(headerBackgroundColor).setTextAlignment(TextAlignment.CENTER)));
 
+        double grandTotal = 0.0;
         // Add items to table
         int slNo = 1;
         for (InvoiceItem item : invoiceItems) {
-            table.addCell(new Cell().add(new Paragraph(String.valueOf(slNo++)).setTextAlignment(TextAlignment.CENTER)));
-            table.addCell(new Cell().add(new Paragraph(item.getItem()).setTextAlignment(TextAlignment.CENTER)));
-            table.addCell(new Cell().add(new Paragraph(item.getQty()).setTextAlignment(TextAlignment.CENTER)));
-            table.addCell(new Cell().add(new Paragraph(item.getRate()).setTextAlignment(TextAlignment.CENTER)));
-            table.addCell(new Cell().add(new Paragraph(String.valueOf(item.getPrice())).setTextAlignment(TextAlignment.CENTER)));
+            table.addCell(createCell(String.valueOf(slNo++), 50, 20));
+            table.addCell(createCell(item.getItem(), 150, 20));
+            table.addCell(createCell(item.getQty(), 80, 20));
+            table.addCell(createCell(item.getRate(), 80, 20));
+            table.addCell(createCell(String.valueOf(item.getPrice()), 100, 20));
+
+            grandTotal += item.getPrice();
         }
         document1.add(table);
 
-        // Add final total
-        String grandTotalText = totalAmountTextView.getText().toString();
-        document1.add(new Paragraph(grandTotalText));
+        Paragraph grandTotalParagraph = new Paragraph("Grand Total:  " + grandTotal)
+                .setBold().setFontSize(16).setMarginLeft(340)
+                .setTextAlignment(TextAlignment.JUSTIFIED)
+                .setMarginTop(20); // Adjust as needed
+        document1.add(grandTotalParagraph);
 
         document1.close();
         outputStream.close();
@@ -256,29 +251,13 @@ public class InvoiceActivity extends AppCompatActivity {
         Toast.makeText(this, "PDF generated successfully", Toast.LENGTH_SHORT).show();
     }
 
-    private Table createHeaderTable() {
-        Table table = new Table(2);
-        table.setWidth(100);
-
-        // Create the first cell and add the first paragraph
-        Cell clientNameCell = new Cell();
-        Paragraph clientNameParagraph = new Paragraph("Client Name: ")
-                .setBold()
-                .setFontSize(14)
-                .setTextAlignment(TextAlignment.LEFT);
-        clientNameCell.add(clientNameParagraph);
-        table.addCell(clientNameCell);
-
-        // Create the second cell and add the second paragraph
-        Cell clientNameValueCell = new Cell();
-        Paragraph clientNameValue = new Paragraph(clientEditText.getText().toString())
-                .setBold()
-                .setFontSize(14)
-                .setTextAlignment(TextAlignment.LEFT);
-        clientNameValueCell.add(clientNameValue);
-        table.addCell(clientNameValueCell);
-
-        return table;
+    private Cell createCell(String content, float width, float height) {
+        return new Cell()
+                .add(new Paragraph(content)
+                        .setMarginTop(10).setFontColor(new DeviceRgb(255, 0, 0))
+                        .setHeight(height)
+                        .setWidth(width)
+                        .setTextAlignment(TextAlignment.CENTER));
     }
 
     @Override
